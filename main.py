d96817d84d7475b292b8e1ceeec1b752bca237c7
Kostik <30mostik@gmail.com>
2026-02-03 15:47:41 +0100

    yes

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ SCREEN_WIDTH = 800
 
 # pygame setup
 pygame.init()
+player_lives = 10
+def monsteranim():
+    global monster_index, monster_surf
+    monster_index += 0.1    
+    if monster_index >= len(monsterpics):
+        monster_index = 0
+    monster_surf = monsterpics[int(monster_index)]
+
+    
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
@@ -13,17 +22,24 @@ player = pygame.Rect((50, 100, 50, 50))  # x, y, width, height
 player_speed = 5
 
 
-monster_surf = pygame.image.load('TheGuy.png').convert_alpha()
-monster_surf = pygame.transform.scale(monster_surf, (monster_surf.get_width()*4, monster_surf.get_height()*4))
-monster_rect = monster_surf.get_rect(midbottom = (100, 200))
+monster_idle = pygame.image.load('TheGuyStandard.png').convert_alpha()
+monster_idle = pygame.transform.scale(monster_idle, (monster_idle.get_width()*4, monster_idle.get_height()*4))
+monster_run1 = pygame.image.load('TheGuyrunning1.png').convert_alpha()
+monster_run1 = pygame.transform.scale(monster_run1, (monster_run1.get_width()*4, monster_run1.get_height()*4))
+monster_run2 = pygame.image.load('TheGuyrunning2.png').convert_alpha()
+monster_run2 = pygame.transform.scale(monster_run2, (monster_run2.get_width()*4, monster_run2.get_height()*4))
+
+monsterpics = [monster_idle, monster_run1, monster_run2]
+monster_index = 0
+monster_surf = monsterpics[monster_index]
 
 monster_x = 50
 monster_y = 300
 monster_speed = 5
 monster_rect = monster_surf.get_rect(midbottom = (monster_x, monster_y)) 
-
-
 # game loop
+time = 0
+lasttime = 0
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -45,6 +61,7 @@ while running:
     screen.fill("black")
 
     monster_rect.right += monster_speed
+    monsteranim()
     if monster_rect.left > SCREEN_WIDTH:
         monster_speed = -monster_speed
     if monster_rect.right < 0:
@@ -53,8 +70,16 @@ while running:
     screen.blit(monster_surf, monster_rect)
     pygame.draw.rect(screen, (255, 0, 0), player)
     # flip() the display to put your work on screen
+    time += clock.get_time()
+    if player.colliderect(monster_rect):
+            if time - lasttime > 2000:
+                lasttime = time
+                player_lives -= 1
+                print(player_lives)
+             
+
     pygame.display.update()
 
-    clock.tick(6000000000000000000)  # limits FPS to 60
+    clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
